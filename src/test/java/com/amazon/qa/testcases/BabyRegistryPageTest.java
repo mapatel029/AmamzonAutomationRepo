6cde7f46de2e6d00026e443d72355fb702fcfd3c
Janish Patel <gamer@Janishs-Air.hitronhub.home>
2023-09-25 13:27:31 -0400

    initial commit

--- src/test/java/com/amazon/qa/testcases/BabyRegistryPageTest.java
+++ src/test/java/com/amazon/qa/testcases/BabyRegistryPageTest.java
@@ -1,5 +1,6 @@
 package com.amazon.qa.testcases;
 
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
@@ -34,7 +35,7 @@ public class BabyRegistryPageTest extends TestBase {
 	}
 
 	@DataProvider
-	public Object[][] getAmazonTestData() {
+	public Object[][] getAmazonTestData() throws InvalidFormatException {
 		Object data[][] = TestUtil.getTestData(sheetName);
 		return data;
 	}
